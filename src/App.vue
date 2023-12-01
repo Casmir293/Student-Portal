65b030a93acb3b15045eeb7efee47be20291c1de
Casmir Onyeka <casmir293@gmail.com>
2023-12-02 00:03:51 +0100

    page setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
+<template>
+  <RouterView />
+</template>
+
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 </script>
-
-<template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template>
--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,23 @@ const router = createRouter({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: () => import("../views/HomeView.vue"),
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("../views/RegisterView.vue"),
+    },
+    {
+      path: "/student",
+      name: "student",
+      component: () => import("../views/StudentView.vue"),
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "notFound",
+      component: () => import("../views/NotFoundView.vue"),
     },
   ],
 });
