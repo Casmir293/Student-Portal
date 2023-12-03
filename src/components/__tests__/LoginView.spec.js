1fcd85e0c95152335198321119d09f95194f69eb
Casmir Onyeka <casmir293@gmail.com>
2023-12-03 15:00:28 +0100

    configured testing for vuetify

--- src/components/__tests__/LoginView.spec.js
+++ src/components/__tests__/LoginView.spec.js
@@ -1,3 +1,19 @@
-import { describe, it, expect } from "vitest";
+import { expect, test } from "vitest";
 import { mount } from "@vue/test-utils";
 import login from "@/views/LoginView.vue";
+import { createVuetify } from "vuetify";
+
+const vuetify = createVuetify();
+
+global.ResizeObserver = require("resize-observer-polyfill");
+
+test("displays message", () => {
+  const wrapper = mount(login, {
+    global: {
+      plugins: [vuetify],
+    },
+  });
+
+  // Assert the rendered text of the component
+  expect(wrapper.text()).toContain("Your expected text here");
+});
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>Hello world</div>
+  <p>Your expected text here</p>
 </template>
 
 <script setup></script>
