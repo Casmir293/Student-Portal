56ccbff4f9a245939740e21d7facb7308c9f55f9
Casmir Onyeka <casmir293@gmail.com>
2023-12-03 20:00:02 +0100

    imported vuetify

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,11 @@
 <template>
   <div>Hello world</div>
   <p>Your expected text here</p>
+  <v-app>
+    <v-container>
+      <v-card title="Card title" text="..." variant="outlined">Vuetify</v-card>
+    </v-container>
+  </v-app>
 </template>
 
 <script setup></script>
