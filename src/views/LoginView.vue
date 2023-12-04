9a4e9e254a83fce80c17d3b847ec0fc131fe54fb
Casmir Onyeka <casmir293@gmail.com>
2023-12-04 22:41:46 +0100

    installed mdi font css for icons

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -16,41 +16,45 @@
         max-width="448"
         rounded="lg"
       >
-        <div class="text-subtitle-1 text-medium-emphasis">Account</div>
-
         <v-text-field
           density="compact"
-          placeholder="Email address"
+          placeholder="Username"
           prepend-inner-icon="mdi-email-outline"
-          variant="outlined"
+          variant="underlined"
+          :rules="[(value) => !!value || 'Username cannot be empty']"
+          required
         ></v-text-field>
 
-        <div
-          class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
-        >
-          Password
+        <v-text-field
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          placeholder="Password"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="underlined"
+          :rules="[(value) => !!value || 'Password cannot be empty']"
+          required
+          @click:append-inner="visible = !visible"
+        ></v-text-field>
 
+        <div>
           <a
             class="text-caption text-decoration-none text-blue"
             href="#"
             rel="noopener noreferrer"
             target="_blank"
           >
-            Forgot login password?</a
+            Forgot Password?</a
           >
         </div>
 
-        <v-text-field
-          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-          :type="visible ? 'text' : 'password'"
-          density="compact"
-          placeholder="Enter your password"
-          prepend-inner-icon="mdi-lock-outline"
-          variant="outlined"
-          @click:append-inner="visible = !visible"
-        ></v-text-field>
-
-        <v-btn block class="mb-8" color="blue" size="large" variant="tonal">
+        <v-btn
+          block
+          class="mb-8 mt-4"
+          color="blue"
+          size="large"
+          variant="tonal"
+        >
           Log In
         </v-btn>
 
