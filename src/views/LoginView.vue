6e71882e4ae4772f7b4544a70fdbeebabd7a6cf5
Casmir Onyeka <casmir293@gmail.com>
2023-12-04 17:07:50 +0100

    added vuetify form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,12 +3,80 @@
     <section class="purple">
       <div><span>Welcome to</span> student portal</div>
       <div>login to access your account</div>
-      <div><img src="" alt="login-img" /></div>
+      <div>
+        <img src="/src/assets/images/banner.png" alt="login-img" />
+      </div>
     </section>
-    <section class="dark"></section>
+    <section class="dark">
+      <h2>Login</h2>
+      <p>Enter your account details</p>
+      <v-card
+        class="mx-auto pa-12 pb-8"
+        elevation="8"
+        max-width="448"
+        rounded="lg"
+      >
+        <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+
+        <v-text-field
+          density="compact"
+          placeholder="Email address"
+          prepend-inner-icon="mdi-email-outline"
+          variant="outlined"
+        ></v-text-field>
+
+        <div
+          class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
+        >
+          Password
+
+          <a
+            class="text-caption text-decoration-none text-blue"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Forgot login password?</a
+          >
+        </div>
+
+        <v-text-field
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          placeholder="Enter your password"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
+          @click:append-inner="visible = !visible"
+        ></v-text-field>
+
+        <v-btn block class="mb-8" color="blue" size="large" variant="tonal">
+          Log In
+        </v-btn>
+
+        <v-card-text class="text-center">
+          <a
+            class="text-blue text-decoration-none"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+          </a>
+        </v-card-text>
+      </v-card>
+    </section>
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
 
-<style lang="scss" scoped></style>
+const visible = ref("false");
+</script>
+
+<style lang="scss" scoped>
+// * {
+//   background-color: yellow;
+// }
+</style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,7 +1,32 @@
 <template>
-  <div></div>
+  <div>
+    <v-sheet width="300" class="mx-auto">
+      <v-form fast-fail @submit.prevent>
+        <v-text-field
+          v-model="firstName"
+          label="Username"
+          :rules="[(value) => !!value || 'Username cannot be empty']"
+          required
+        ></v-text-field>
+
+        <v-text-field
+          v-model="lastName"
+          label="Password"
+          :rules="[(value) => !!value || 'Password cannot be empty']"
+          required
+        ></v-text-field>
+
+        <v-btn type="submit" block class="mt-2">Submit</v-btn>
+      </v-form>
+    </v-sheet>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const firstName = ref("");
+const lastName = ref("");
+</script>
 
 <style lang="scss" scoped></style>
