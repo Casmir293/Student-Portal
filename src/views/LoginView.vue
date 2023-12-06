1d5b45838411e043d8c6b8fb3848e90c5c918695
Casmir Onyeka <casmir293@gmail.com>
2023-12-06 11:05:29 +0100

    initial ipad commit

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,108 +1,104 @@
 <template>
-  <section>
-    <v-row justify="center">
-      <v-col cols="12" md="8">
-        <v-card class="pt-8 text-center" color="myPurple" rounded="0">
-          <v-card-item>
-            <v-card-title
-              ><div class="text-h4 pb-2">
-                <span class="font-weight-bold">Welcome to</span><br />
-                student portal
-              </div></v-card-title
-            >
-
-            <v-card-subtitle
-              ><div class="font-weight-medium text-subtitle-2">
-                login to access your account
-              </div></v-card-subtitle
-            >
-          </v-card-item>
-
-          <v-card-text>
-            <div>
-              <img src="/src/assets/images/banner.png" alt="login-img" />
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-
-    <!-- <section class="purple">
-      <div>
-        <span class="font-weight-bold">Welcome to</span><br />
-        student portal
-      </div>
-      <div class="font-weight-medium">login to access your account</div>
-      <div>
-        <img src="/src/assets/images/banner.png" alt="login-img" />
-      </div>
-    </section> -->
-    <section class="dark">
-      <v-card
-        class="mx-auto pa-12 pb-8 text-white bg-myBlack"
-        elevation="8"
-        max-width="448"
-        rounded="0"
-      >
-        <h2 class="pb-2 font-weight-bold text-h4">Login</h2>
-        <p class="pb-4 font-weight-medium text-subtitle-1">
-          Enter your account details
-        </p>
-        <v-text-field
-          placeholder="Username"
-          prepend-inner-icon="mdi-account-outline"
-          variant="underlined"
-          :rules="[(value) => !!value || 'Username cannot be empty']"
-          required
+  <section class="content">
+    <main>
+      <!-- Dark Section -->
+      <section class="dark-section bg-myBlack">
+        <v-card
+          class="mx-auto pa-12 pb-8 text-white bg-myBlack"
+          elevation="8"
+          max-width="448"
+          rounded="0"
         >
-        </v-text-field>
-
-        <v-text-field
-          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-          :type="visible ? 'text' : 'password'"
-          placeholder="Password"
-          prepend-inner-icon="mdi-lock-outline"
-          variant="underlined"
-          :rules="[(value) => !!value || 'Password cannot be empty']"
-          required
-          @click:append-inner="visible = !visible"
-        ></v-text-field>
-
-        <div>
-          <a
-            class="text-caption text-decoration-none text-white"
-            href="#"
-            rel="noopener noreferrer"
-            target="_blank"
+          <h2 class="pb-2 font-weight-bold text-h4">Login</h2>
+          <p class="pb-4 font-weight-medium text-subtitle-1">
+            Enter your account details
+          </p>
+          <v-text-field
+            placeholder="Username"
+            prepend-inner-icon="mdi-account-outline"
+            variant="underlined"
+            :rules="[(value) => !!value || 'Username cannot be empty']"
+            required
           >
-            Forgot Password?</a
-          >
-        </div>
+          </v-text-field>
 
-        <v-btn
-          block
-          class="mb-15 mt-8 text-capitalize"
-          size="large"
-          color="myPurple"
-        >
-          Login
-        </v-btn>
+          <v-text-field
+            :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+            :type="visible ? 'text' : 'password'"
+            placeholder="Password"
+            prepend-inner-icon="mdi-lock-outline"
+            variant="underlined"
+            :rules="[(value) => !!value || 'Password cannot be empty']"
+            required
+            @click:append-inner="visible = !visible"
+          ></v-text-field>
 
-        <div
-          class="text-caption d-flex align-center justify-space-between mb-10"
-        >
-          <span class="text-white">Don't have an account?</span>
-
-          <v-btn class="mt" color="grey-darken-3" size="small">
-            <router-link
-              to="/register"
-              class="text-white text-decoration-none text-capitalize"
-              >Sign up</router-link
+          <div>
+            <a
+              class="text-caption text-decoration-none text-white"
+              href="#"
+              rel="noopener noreferrer"
+              target="_blank"
             >
+              Forgot Password?</a
+            >
+          </div>
+
+          <v-btn
+            block
+            class="mb-15 mt-8 text-capitalize"
+            size="large"
+            color="myPurple"
+          >
+            Login
           </v-btn>
-        </div>
-      </v-card>
-    </section>
+
+          <div
+            class="text-caption d-flex align-center justify-space-between mb-10"
+          >
+            <span class="text-white">Don't have an account?</span>
+
+            <v-btn class="mt" color="grey-darken-3" size="small">
+              <router-link
+                to="/register"
+                class="text-white text-decoration-none text-capitalize"
+                >Sign up</router-link
+              >
+            </v-btn>
+          </div>
+        </v-card>
+      </section>
+
+      <!-- Purple Section -->
+      <section class="purple-section bg-myPurple">
+        <v-row justify="center">
+          <v-col cols="12" md="8">
+            <v-card class="pt-8 text-center" color="myPurple" rounded="0">
+              <v-card-item>
+                <v-card-title
+                  ><div class="text-h4 pb-2">
+                    <span class="font-weight-bold">Welcome to</span><br />
+                    student portal
+                  </div></v-card-title
+                >
+
+                <v-card-subtitle
+                  ><div class="font-weight-medium text-subtitle-2">
+                    login to access your account
+                  </div></v-card-subtitle
+                >
+              </v-card-item>
+
+              <v-card-text>
+                <div>
+                  <img src="/src/assets/images/banner.png" alt="login-img" />
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </section>
+    </main>
   </section>
 </template>
 
@@ -115,6 +111,41 @@ const visible = ref(false);
 <style lang="scss" scoped>
 * {
   font-family: "poppins", sans-serif;
-  font-weight: 400px;
+  font-weight: 400;
+}
+
+main {
+  display: flex;
+  flex-direction: column-reverse;
+}
+
+// Media Query for Ipad
+@media (min-width: 576px) {
+  main {
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  .dark-section {
+    height: 100vh;
+    width: 100%;
+  }
+
+  .purple-section {
+    height: 100vh;
+    width: 100%;
+  }
+}
+
+// Media Query for Laptops
+@media (min-width: 992px) {
 }
 </style>
