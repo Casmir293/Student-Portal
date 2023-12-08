f5b0bd5a8527f4214db24c7a3500f645e373352b
Casmir Onyeka <casmir293@gmail.com>
2023-12-08 09:46:30 +0100

    added template to register page

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,105 +1,165 @@
 <template>
-  <div>
-    <v-sheet width="300" class="mx-auto">
-      <v-form fast-fail @submit.prevent>
-        <v-text-field
-          v-model="firstName"
-          label="Username"
-          :rules="[(value) => !!value || 'Username cannot be empty']"
-          required
-        ></v-text-field>
-
-        <v-text-field
-          v-model="lastName"
-          label="Password"
-          :rules="[(value) => !!value || 'Password cannot be empty']"
-          required
-        ></v-text-field>
-
-        <v-btn type="submit" block class="mt-2">Submit</v-btn>
-      </v-form>
-    </v-sheet>
-  </div>
-
-  <div>
-    <v-img
-      class="mx-auto my-6"
-      max-width="228"
-      src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"
-    ></v-img>
-
-    <v-card
-      class="mx-auto pa-12 pb-8"
-      elevation="8"
-      max-width="448"
-      rounded="lg"
-    >
-      <div class="text-subtitle-1 text-medium-emphasis">Account</div>
-
-      <v-text-field
-        density="compact"
-        placeholder="Email address"
-        prepend-inner-icon="mdi-email-outline"
-        variant="outlined"
-      ></v-text-field>
-
-      <div
-        class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
-      >
-        Password
-
-        <a
-          class="text-caption text-decoration-none text-blue"
-          href="#"
-          rel="noopener noreferrer"
-          target="_blank"
+  <section class="content">
+    <main>
+      <!-- Dark Section -->
+      <section class="dark-section bg-myBlack">
+        <v-card
+          class="mx-auto pa-12 pb-8 text-white bg-myBlack"
+          elevation="8"
+          max-width="600"
+          rounded="0"
         >
-          Forgot login password?</a
+          <h2 class="pb-2 font-weight-bold text-h4">Login</h2>
+          <p class="pb-4 font-weight-medium text-subtitle-1">
+            Enter your account details
+          </p>
+          <v-text-field
+            placeholder="Username"
+            prepend-inner-icon="mdi-account-outline"
+            variant="underlined"
+            :rules="[(value) => !!value || 'Username cannot be empty']"
+            required
+          >
+          </v-text-field>
+
+          <v-text-field
+            :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+            :type="visible ? 'text' : 'password'"
+            placeholder="Password"
+            prepend-inner-icon="mdi-lock-outline"
+            variant="underlined"
+            :rules="[(value) => !!value || 'Password cannot be empty']"
+            required
+            @click:append-inner="visible = !visible"
+          ></v-text-field>
+
+          <div>
+            <a
+              class="text-caption text-decoration-none text-white"
+              href="#"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Forgot Password?</a
+            >
+          </div>
+
+          <v-btn
+            block
+            class="mb-15 mt-8 text-capitalize"
+            size="large"
+            color="myPurple"
+          >
+            Login
+          </v-btn>
+
+          <div
+            class="text-caption d-flex align-center justify-space-between mb-10"
+          >
+            <span class="text-white">Don't have an account?</span>
+
+            <v-btn class="mt" color="grey-darken-3" size="small">
+              <router-link
+                to="/register"
+                class="text-white text-decoration-none text-capitalize"
+                >Sign up</router-link
+              >
+            </v-btn>
+          </div>
+        </v-card>
+      </section>
+
+      <!-- Purple Section -->
+      <section class="purple-section bg-myPurple px-sm-4">
+        <v-card
+          class="pt-8 text-center"
+          color="myPurple"
+          rounded="0"
+          :elevation="0"
         >
-      </div>
+          <v-card-item class="pa-0">
+            <v-card-title
+              ><div class="text-h4 text-sm-h3 text-md-h2 pb-2 text-left pl-4">
+                <span class="font-weight-black">Welcome to</span><br />
+                <span class="text-h5 text-sm-h4 text-md-h3"
+                  >student portal</span
+                >
+              </div></v-card-title
+            >
 
-      <v-text-field
-        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-        :type="visible ? 'text' : 'password'"
-        density="compact"
-        placeholder="Enter your password"
-        prepend-inner-icon="mdi-lock-outline"
-        variant="outlined"
-        @click:append-inner="visible = !visible"
-      ></v-text-field>
+            <v-card-subtitle
+              ><div
+                class="font-weight-medium text-subtitle-1 text-left pb-sm-5 pl-4"
+              >
+                login to access your account
+              </div></v-card-subtitle
+            >
+          </v-card-item>
 
-      <v-card class="mb-12" color="surface-variant" variant="tonal">
-        <v-card-text class="text-medium-emphasis text-caption">
-          Warning: After 3 consecutive failed login attempts, you account will
-          be temporarily locked for three hours. If you must login now, you can
-          also click "Forgot login password?" below to reset the login password.
-        </v-card-text>
-      </v-card>
-
-      <v-btn block class="mb-8" color="blue" size="large" variant="tonal">
-        Log In
-      </v-btn>
-
-      <v-card-text class="text-center">
-        <a
-          class="text-blue text-decoration-none"
-          href="#"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
-        </a>
-      </v-card-text>
-    </v-card>
-  </div>
+          <v-card-text class="pa-sm-0">
+            <div>
+              <img src="/src/assets/images/banner.png" alt="login-img" />
+            </div>
+          </v-card-text>
+        </v-card>
+      </section>
+    </main>
+  </section>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-const firstName = ref("");
-const lastName = ref("");
 const visible = ref(false);
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+* {
+  font-family: "poppins", sans-serif;
+  font-weight: 400;
+  box-sizing: border-box;
+}
+
+main {
+  display: flex;
+  flex-direction: column-reverse;
+}
+
+// Media Query for Ipad
+@media (min-width: 576px) {
+  main {
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    background-color: #2937b1;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  .dark-section {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .purple-section {
+    width: 100%;
+  }
+
+  img {
+    width: 80%;
+    height: auto;
+  }
+}
+
+// Media Query for Laptops
+@media (min-width: 992px) {
+}
+</style>
