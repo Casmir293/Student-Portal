dcf2f272407ba6c8c00588b496a7891026f26aa8
Casmir Onyeka <casmir293@gmail.com>
2023-12-28 18:23:26 +0100

    used v-card for the success alert

--- src/components/SuccessfulComp.vue
+++ src/components/SuccessfulComp.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="text-center">
-    <img
-      src="../assets/images/successful.png"
-      alt="successful"
-      class="w-50 h-auto"
-    />
-    <p>
-      <span>Registration successful!</span> <br />
-      Please check your email for verification.
-    </p>
+    <v-card width="400">
+      <v-card-text>
+        <img
+          src="../assets/images/successful.png"
+          alt="successful"
+          class="w-50 h-auto"
+        />
+      </v-card-text>
+      <v-card-item>
+        <v-card-title>Registration successful!</v-card-title>
 
-    <router-link to="/">Exit</router-link>
+        <v-card-subtitle
+          >Please check your email for verification.</v-card-subtitle
+        >
+      </v-card-item>
+
+      <v-card-actions>
+        <v-btn> <router-link to="/">Exit</router-link> </v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
