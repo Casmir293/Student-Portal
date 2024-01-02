12abd565b8fc742c5b913389736e3ccbe9feac3f
Casmir Onyeka <casmir293@gmail.com>
2024-01-02 18:31:51 +0100

    finished mobile header

--- src/views/StudentView.vue
+++ src/views/StudentView.vue
@@ -1,6 +1,21 @@
 <template>
   <section>
-    <section class="purple-section bg-myPurple px-sm-4"></section>
+    <section>
+      <v-app>
+        <v-container>
+          <v-app-bar :elevation="2" class="bg-myPurple px-sm-4 justify-end px-3"
+            ><v-avatar color="deep-purple-darken-4">
+              <v-icon size="40" icon="mdi-account-circle"></v-icon>
+            </v-avatar>
+            <div class="px-3">
+              <div class="text-caption">Welcome</div>
+              <div class="font-weight-bold">Casmir Onyeka</div>
+            </div>
+            <v-icon icon="mdi-logout"></v-icon>
+          </v-app-bar>
+        </v-container>
+      </v-app>
+    </section>
   </section>
 </template>
 
