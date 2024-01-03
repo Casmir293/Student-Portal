f251ec3ff5cf2f5749900586221b9207b17ae1e4
Casmir Onyeka <casmir293@gmail.com>
2024-01-03 20:40:51 +0100

    finished hero section

--- src/views/StudentView.vue
+++ src/views/StudentView.vue
@@ -2,6 +2,7 @@
   <section>
     <section>
       <v-app>
+        <!-- header -->
         <div
           :elevation="2"
           class="bg-myPurple px-sm-4 py-2 px-3 d-flex align-center justify-end"
@@ -15,6 +16,32 @@
           </div>
           <v-icon icon="mdi-logout"></v-icon>
         </div>
+
+        <!-- hero -->
+        <v-card class="bg-myPurple rounded-0 pt-4">
+          <v-card-item>
+            <div class="text-overline mb-1 text-grey">January 2, 2024</div>
+            <div class="text-h6 mb-1 font-weight-bold text-center">
+              Welcome back, Casmir!
+            </div>
+            <div class="text-caption text-grey text-center">
+              Always stay updated in your student portal
+            </div>
+            <div class="position-relative pt-3">
+              <img
+                src="../assets/images/student.png"
+                alt="student"
+                class="w-100"
+              />
+              <img
+                src="../assets/images/Scholarcap.png"
+                alt="Scholarcap"
+                class="w-50 position-absolute"
+                style="left: -5%; bottom: 35%"
+              />
+            </div>
+          </v-card-item>
+        </v-card>
       </v-app>
     </section>
   </section>
