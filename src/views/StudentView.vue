d66c15bdf2a7c7f037beedcdf2591d8196b80a0a
Casmir Onyeka <casmir293@gmail.com>
2024-01-03 19:46:22 +0100

    modified header

--- src/views/StudentView.vue
+++ src/views/StudentView.vue
@@ -2,18 +2,19 @@
   <section>
     <section>
       <v-app>
-        <v-container>
-          <v-app-bar :elevation="2" class="bg-myPurple px-sm-4 justify-end px-3"
-            ><v-avatar color="deep-purple-darken-4">
-              <v-icon size="40" icon="mdi-account-circle"></v-icon>
-            </v-avatar>
-            <div class="px-3">
-              <div class="text-caption">Welcome</div>
-              <div class="font-weight-bold">Casmir Onyeka</div>
-            </div>
-            <v-icon icon="mdi-logout"></v-icon>
-          </v-app-bar>
-        </v-container>
+        <div
+          :elevation="2"
+          class="bg-myPurple px-sm-4 py-2 px-3 d-flex align-center justify-end"
+        >
+          <v-avatar color="deep-purple-darken-4">
+            <v-icon size="40" icon="mdi-account-circle"></v-icon>
+          </v-avatar>
+          <div class="px-3">
+            <div class="text-caption">Welcome</div>
+            <div class="font-weight-bold">Casmir Onyeka</div>
+          </div>
+          <v-icon icon="mdi-logout"></v-icon>
+        </div>
       </v-app>
     </section>
   </section>
