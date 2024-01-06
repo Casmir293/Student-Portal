538af5fa5af33da4ed1a8149f9314b29c6f9659c
Casmir Onyeka <casmir293@gmail.com>
2024-01-06 14:04:46 +0100

    ipad hero

--- src/views/StudentView.vue
+++ src/views/StudentView.vue
@@ -18,30 +18,32 @@
         </div>
 
         <!-- hero -->
-        <v-card class="bg-myPurple rounded-0 pt-4">
-          <v-card-item>
-            <div class="text-overline mb-1 text-grey">January 2, 2024</div>
-            <div class="text-h6 mb-1 font-weight-bold text-center">
-              Welcome back, Casmir!
-            </div>
-            <div class="text-caption text-grey text-center">
-              Always stay updated in your student portal
+
+        <section class="bg-myPurple rounded-0 pt-4">
+          <div class="hero">
+            <div>
+              <div class="text-overline mb-1 text-grey">January 2, 2024</div>
+              <div class="text-h6 mb-1 font-weight-bold text-center">
+                Welcome back, Casmir!
+              </div>
+              <div class="text-caption text-grey text-center">
+                Always stay updated in your student portal
+              </div>
             </div>
             <div class="position-relative pt-3">
               <img
                 src="../assets/images/student.png"
                 alt="student"
-                class="w-100"
+                class="student-img w-100"
               />
               <img
                 src="../assets/images/Scholarcap.png"
                 alt="Scholarcap"
-                class="w-50 position-absolute"
-                style="left: -5%; bottom: 35%"
+                class="cap-img w-50 position-absolute"
               />
             </div>
-          </v-card-item>
-        </v-card>
+          </div>
+        </section>
 
         <!-- content -->
         <section class="px-4 py-8 bg-grey-lighten-4">
@@ -2085,5 +2087,20 @@
 <style lang="scss" scoped>
 .enrolled {
   border: 3px solid #2937b1;
+}
+
+.cap-img {
+  left: -5%;
+  bottom: 35%;
+}
+
+// Media Query for Ipad
+@media only screen and (min-width: 600px) {
+  .hero {
+    display: grid;
+    grid-template-columns: 60% 40%;
+    align-items: center;
+    justify-items: start;
+  }
 }
 </style>
